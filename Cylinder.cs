8ae97c2187b41156fa8a65233f2c9d29fff191b1
lenhat41555 <70925729+lenhat41555@users.noreply.github.com>
2020-09-22 11:51:29 +0700

    Initial commit

---
+++ Cylinder.cs
@@ -0,0 +1,38 @@
+
+    using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Circle_Cylinder
+{
+    public class Cylinder : Circle
+    {
+        private double Height;
+
+        public Cylinder(double height,double R, string Color):base(R,Color)
+        {
+            Height = height;
+        }
+        public Cylinder()
+        {
+
+        }
+
+        public void setHeight(double height)
+        {
+            Height = height;
+        }
+        public double getHeight()
+        {
+            return Height;
+        }
+        
+        public double Volume()
+        {
+            return Area * Height;
+        }
+        public override string ToString()
+        {
+            return"Height = "+ getHeight() + " The tich = "+ Volume() + base.ToString();
+        }
+    }
+}
